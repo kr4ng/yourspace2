ae7aec1de9fc246b39737ec69147acde8b6bf825
kr4ng <steven.m.simoni@gmail.com>
2014-04-06 18:05:18 -0700

    added first templates

--- website/views.py
+++ website/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+from website.models import Person
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello World!!')
+    names = Person.objects.all()
+    template = loader.get_template('website/index.html')
+    context = RequestContext(request, {
+        'names': names,
+    })
+    return HttpResponse(template.render(context))
