ff327e3155ae71a927706f4d643bb98bf14656b9
kr4ng <steven.m.simoni@gmail.com>
2014-04-24 11:41:09 -0700

    added support for multiple pages and content

--- yourspace2/urls.py
+++ yourspace2/urls.py
@@ -13,5 +13,11 @@ urlpatterns = patterns("",
     
     # There's no favicon here!
     url(r"^favicon.ico$", generic.RedirectView.as_view()),
-    url(r'^$', 'website.views.home', name='home'),    
+    url(r'^$', 'website.views.home', name='home'),
+    url(r'^about$', 'website.views.about', name='about'),
+    url(r'^photos$', 'website.views.photos', name='photos'),
+    url(r'^pricing$', 'website.views.pricing', name='pricing'),
+    url(r'^contact$', 'website.views.contact', name='contact'),
+    url(r'^reserve$', 'website.views.about', name='reserve'),
+    url(r'^index$', 'website.views.home', name='index'),    
 )
